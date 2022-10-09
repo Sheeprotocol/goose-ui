bab5a21394cad853141e61834650f7b9a4cc8588
rylandkoopus <ryland.koopus@gmail.com>
2022-10-09 17:27:44 -0400

    updated colors again

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2865,20 +2865,20 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
-    secondary: "#7645D9",
+    primary: "#aa2929",
+    primaryBright: "#ef2727",
+    primaryDark: "#761212",
+    secondary: "#a53535",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#faf9f9", backgroundDisabled: "#ebe9e9", contrast: "#261313", invertedContrast: "#ffffff", input: "#f4eaea", tertiary: "#f5efef", text: "#7a2a2a", textDisabled: "#c4bdbd", textSubtle: "#ba8080", borderColor: "#ebe9e9", card: "#ffffff", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#faf9f9", backgroundDisabled: "#ebe9e9", contrast: "#261313", invertedContrast: "#ffffff", input: "#f4eaea", tertiary: "#f5efef", text: "#7a2a2a", textDisabled: "#c4bdbd", textSubtle: "#b43737", borderColor: "#ebe9e9", card: "#ffffff", gradients: {
         bubblegum: "linear-gradient(139.73deg, #ffe6e6 0%, #ffefef 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ff2020", background: "#353131", backgroundDisabled: "#423737", contrast: "#ffffff", invertedContrast: "#261313", input: "#5a3f3f", primaryDark: "#a10000", tertiary: "#473535", text: "#fce2e2", textDisabled: "#716161", textSubtle: "#d4c4c4", borderColor: "#634b4b", card: "#2c2626", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ff2020", background: "#000000", backgroundDisabled: "#1f1a1a", contrast: "#ffffff", invertedContrast: "#261313", input: "#5a3f3f", primaryDark: "#a10000", tertiary: "#473535", text: "#ff3e3e", textDisabled: "#7f4242", textSubtle: "#d4c4c4", borderColor: "#634b4b", card: "#323232", gradients: {
         bubblegum: "linear-gradient(139.73deg, #5c3131 0%, #542a2a 100%)",
     } });
 
