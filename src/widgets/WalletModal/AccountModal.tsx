96909b113da55c6ef2b3a596395179ef6eb51dda
hypercoredex <coredexdotxyz@proton.me>
2023-05-23 17:17:10 -0400

    update bscscan text to pulsescan

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -24,7 +24,7 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
     </Text>
     <Flex mb="32px">
       <LinkExternal small href={`https://scan.pulsechain.com/address/${account}`} mr="16px">
-        View on BscScan
+        View on PulseScan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
